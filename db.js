2e336a2de7df5bf8b09873a1cdf0d3130ffaabae
37anjali <anjali.anchaurasiya210@gmail.com>
2024-10-15 14:42:22 +0530

    first commit

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ function connectToDB(){
     mongoose.connect(process.env.MONGO_URI)
     
   .then(() => console.log('mongodb is connected successfully'))
-  .catch(() => console.log('error in connection mongodb'));
+  .catch(() => console.log('error in connection mongodb'))
   
 
 }
