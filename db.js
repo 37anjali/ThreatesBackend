67cf6501c1534f3d30f06ca8692182e75fce01dc
37anjali <anjali.anchaurasiya210@gmail.com>
2024-10-15 13:04:10 +0530

    update

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config()
 
 function connectToDB(){
-    mongoose.connect('mongodb://127.0.0.1:27017/blogapp')
+    mongoose.connect(process.env.MONGO_URI)
+    
   .then(() => console.log('mongodb is connected successfully'))
   .catch(() => console.log('error in connection mongodb'));
   
